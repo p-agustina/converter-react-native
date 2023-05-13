576a782b0feef131a6f2bc735609b5938ac564c5
AGUSTINA PATT <agustinapatt@gmail.com>
2023-05-13 14:40:43 +0100

    convertions working

--- components/InputBox.jsx
+++ components/InputBox.jsx
@@ -2,18 +2,15 @@ import { View, Text, TextInput, StyleSheet } from "react-native";
 
 export default InputBox = (props) => {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.title}>{props.title}</Text>
+    <View >
+      <View style={styles.container}>
+        <Text>{props.title}</Text>
         <TextInput
           placeholder="Type here!"
-          onChangeText={(newText) => {
-            props.setText(newText);
+          onChangeText={(newValue) => {
+            props.setValue(newValue);
           }}
         />
-      </View>
-      <View>
-        <Text>Hola</Text>
       </View>
     </View>
   );
@@ -25,8 +22,8 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
   },
-  // container: {
-  //     flex: 3,
-  //     flexDirection: "row"
-  // }
+  container: {
+      justifyContent: "space-around",
+      padding: 10,
+  }
 });
--- screens/ConverterScreen.jsx
+++ screens/ConverterScreen.jsx
@@ -1,19 +1,52 @@
 import { View, StyleSheet, Text} from "react-native";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import InputBox from "../components/InputBox";
 import DropdownMenu from "../components/DropdownMenu";
 
 export default ConverterScreen = () => {
-  const [text, setText] = useState("");
+  const [value, setValue] = useState(0);
   const [selected, setSelected] = useState("");
+  const [convertedValue, setConvertedValue] = useState(0);
+  const [convertedUnit, setConvertedUnit] = useState("");
+
+  function Convertion() {
+    switch(selected) {
+      case "ounce":
+        setConvertedValue(value * 28.3495)
+        setConvertedUnit("grams")
+      break;
+      case "Fahrenheit": 
+        setConvertedValue((value - 32) * 5/9)
+        setConvertedUnit("Celsius")
+      break;
+      case "cup":
+        setConvertedValue(value * 240)
+        setConvertedUnit("mililiter")
+      break;
+      case "inches":
+        setConvertedValue(value * 2.54)
+        setConvertedUnit(centimeters)
+    }
+  };
+
+  useEffect(() => {
+    Convertion()
+  }, [value]);
+
+  useEffect(() => {
+    setValue(null);
+    setConvertedUnit(null)
+  }, [selected])
 
 
   return (
     <View style={styles.layout}>
-    <Text>Selected: {selected}</Text>
       <DropdownMenu setSelected={setSelected}/>
+      <View style={styles.input}>
+        <InputBox title={selected} setValue={setValue} />
+      </View>
       <View>
-        <InputBox title={selected} setText={setText} />
+        <Text>{convertedValue} {convertedUnit}</Text>
       </View>
     </View>
   );
@@ -24,5 +57,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     padding: 8,
+  },
+  input: {
+    margin: 10,
   }
 });
